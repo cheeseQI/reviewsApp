cc7551f45aec0d5fb116813b438c19cbe27e91c8
sq44 <sq44@duke.edu>
2022-11-10 22:14:54 -0500

    first commit

---
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -0,0 +1,9 @@
+package com.hmdp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmDianPingApplicationTests {
+    // back: http://localhost:8081/shop-type/list
+    // front: http://localhost:8080/
+}
